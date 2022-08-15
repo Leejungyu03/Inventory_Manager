e17e54fb018af71141f645fa5f771bc037f865fb
unknown <l1j1g4@gmail.com>
2022-08-16 00:24:21 +0900

    Feat: 메인 Product 리스트 추가

--- src/main/java/com/manager/main/MainController.java
+++ src/main/java/com/manager/main/MainController.java
@@ -1,24 +1,36 @@
 package com.manager.main;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.manager.product.bo.ProductBO;
+import com.manager.product.model.Product;
+
 @Controller
 public class MainController {
 
+  @Autowired
+  private ProductBO productBO;
+
   @RequestMapping("/main")
-  public String Main(
-    Model model,
-    HttpSession session) {
-    model.addAttribute("viewName", "main/Main");
+  public String Main(Model model, HttpSession session) {
+
     String loginId = (String)session.getAttribute("loginId");
 
     if (loginId == null) {
       return "redirect:/user/sign_in_view";
     }
+
+    List<Product> product = productBO.getProduct();
+
+    model.addAttribute("viewName", "main/Main");
+    model.addAttribute("productList", product);
 
     return "template/Layout";
   }

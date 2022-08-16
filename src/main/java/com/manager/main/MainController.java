b96754d6c847c9da3575e02f99a1f063dc49a352
Leejungyu03 <l1j1g4@gmail.com>
2022-08-16 17:56:42 +0900

    Feat: 메인 UI 기능 추가

--- src/main/java/com/manager/main/MainController.java
+++ src/main/java/com/manager/main/MainController.java
@@ -12,25 +12,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.manager.product.bo.ProductBO;
 import com.manager.product.model.Product;
 
+@RequestMapping("/manager")
 @Controller
 public class MainController {
 
   @Autowired
   private ProductBO productBO;
 
-  @RequestMapping("/main")
+  @RequestMapping("/main_view")
   public String Main(Model model, HttpSession session) {
 
     String loginId = (String)session.getAttribute("loginId");
+    Integer userId = (Integer)session.getAttribute("userId");
 
     if (loginId == null) {
       return "redirect:/user/sign_in_view";
     }
 
-    List<Product> product = productBO.getProduct();
+    List<Product> productList = productBO.getProduct();
 
+    model.addAttribute("title", "상품 재고 관리");
     model.addAttribute("viewName", "main/Main");
-    model.addAttribute("productList", product);
+    model.addAttribute("productList", productList);
+    model.addAttribute("userId", userId);
 
     return "template/Layout";
   }
--- src/main/resources/static/js/main/Main.js
+++ src/main/resources/static/js/main/Main.js
@@ -1,3 +1,28 @@
 $(document).ready(function() {
 	
-})
+  $('.modify-btn').click(function() {
+    let productId = $(this).data('product-id');
+    location.href = "/product/update_view?productId=" + productId;
+  });
+
+  $('.delete-btn').click(function() {
+    let productId = $(this).data('product-id');
+
+    $.ajax ({
+      type : "DELETE",
+      url : "/product/delete",
+      data : {"productId" : productId},
+      success : function(data) {
+        if (data.result === 'success') {
+          alert("게시물 삭제되었습니다.");
+          location.reload();
+        } else {
+          alert("게시물 삭제에 실패했습니다.");
+        }
+      },
+      error : function(e) {
+        alert("게시물 삭제에 실패햇습니다.");
+      }
+    });
+  });
+});
